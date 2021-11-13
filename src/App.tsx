010bb4b92d4a4e4609d87b16c9cf4078478f4bfe
samlee <samuelwjlee@gmail.com>
2021-11-13 14:18:59 -0800

    print perf logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import './App.css';
+import { useEffect } from 'react'
+import './App.css'
+import reportWebVitals from './reportWebVitals'
 
-function getResourceEntry() {
-  const metric = window.performance.getEntriesByType('resource')[0];
+function getPerformanceEntry(): any {
+  const perf = performance.getEntriesByType('navigation') || []
+  return perf[0]
+}
 
-  return JSON.stringify(metric);
+function LogRow({ label, val }: { label: string; val: number | string }) {
+  return (
+    <div className="Log">
+      <strong>{'> '}</strong>
+      <span>{`${label}: `}</span>
+      <span>{typeof val === 'number' ? `${val} ms` : val}</span>
+    </div>
+  )
 }
 
 function App() {
+  const perf = getPerformanceEntry()
+
+  useEffect(() => {
+    console.log(performance.getEntries())
+    reportWebVitals((report) => {
+      console.log(report)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <div className="Code">
-        {getResourceEntry()}
+      <div className="Terminal">
+        <LogRow label={'name'} val={perf.name} />
+        <LogRow label={'startTime'} val={perf.startTime} />
+        <LogRow label={'duration'} val={perf.duration} />
+        <LogRow label={'TTI'} val={perf.domInteractive - perf.requestStart} />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
