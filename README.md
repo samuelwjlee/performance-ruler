4087742374f95513e15e340813a0b676dc9ed22a
samlee <samuelwjlee@gmail.com>
2021-11-14 13:40:20 -0800

    Add custom hook

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
-# web-vital
+# performance-ruler
 
-The purpose of this repo is to visualize what would be sent to measure page performance analytics.
+Measure client-side page loading speed using `performance.mark` and `performance.measure`.
 
-![demo](./src/demo.png)
+👇🏼 Custom hook defined to dynamically "hook" markers and measure func
+
+```javascript
+function usePerformanceMarker(name: string) {
+  const start = `${name}-start`
+  const end = `${name}-end`
+  return {
+    startMarker: () => performance.mark(start),
+    endMarker: () => performance.mark(end),
+    measure: () => performance.measure(name, start, end),
+  }
+}
+```
+
+👇🏼 Consume perusePerformanceMarker like this
+
+```javascript
+const { startMarker } = usePerformanceMarker('page-load')
+
+function App() {
+  function navigate() {
+    startMarker()
+  }
+  return <button onClick={navigate} />
+}
+
+function Page() {
+  const { endMarker, measure } = usePerformanceMarker('page-load')
+
+  endMarker()
+  measure()
+
+  return <div>hello world</div>
+}
+```
+
+👇🏼 this is would could be sent to analytics if desired
+
+```json
+{
+  "name": "page-load",
+  "entryType": "measure",
+  "startTime": 171.69999998807907,
+  "duration": 1006.6999999880791
+}
+```
+
+![demo](./src/demo.gif)
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import React, { useEffect, useState } from 'react'
+import './index.css'
+
+const MOCK_API_RESPONSE_SPEED = 1000
+
+/**
+ * hook performance marker anywhere with custom name
+ */
+function usePerformanceMarker(name: string) {
+  const start = `${name}-start`
+  const end = `${name}-end`
+  return {
+    startMarker: () => performance.mark(start),
+    endMarker: () => performance.mark(end),
+    /* this func could fire analytics if desired */
+    measure: () => performance.measure(name, start, end),
+  }
+}
+
+function Terminal({ loading }: { loading: boolean }) {
+  const [metric, setMetric] = useState('loading...')
+  const { measure, endMarker } = usePerformanceMarker('page-load')
+
+  useEffect(() => {
+    if (!loading) {
+      /**
+       * loading complete 🎉
+       * At this point, the page should be fully interactive
+       */
+      endMarker()
+
+      setMetric(JSON.stringify(measure(), null, 2))
+    }
+  }, [loading])
+
+  return (
+    <code className="Code">
+      <pre>{metric}</pre>
+    </code>
+  )
+}
+
+function App() {
+  const [loaded, setLoaded] = useState(false)
+  const { startMarker } = usePerformanceMarker('page-load')
+
+  let timeoutId: NodeJS.Timeout | null = null
+
+  useEffect(() => {
+    /**
+     * loading started 🏁.!
+     * this could be when user hits landing page url,
+     * clicks a button to navigate to a page, etc
+     */
+    startMarker()
+
+    timeoutId = setTimeout(() => {
+      setLoaded((loaded) => !loaded)
+
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+      }
+    }, MOCK_API_RESPONSE_SPEED)
+  }, [])
+
+  return (
+    <div className="App">
+      <Terminal loading={!loaded} />
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
